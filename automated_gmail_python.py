c65d3f7cc614329bac1677e06044bc6d23c4f815
Rohit <Rohit.defteam@gmail.com>
2022-10-26 12:59:48 +0530

    commit2

--- automated_gmail_python.py
+++ automated_gmail_python.py
@@ -3,8 +3,8 @@ def MailSender(mail):
         import smtplib
         connection = smtplib.SMTP("SMTP.gmail.com",587)
         connection.starttls()
-        connection.login(user = "",password = "")
-        connection.sendmail(from_addr = "",to_addrs = email, msg = "Subject:Very Fresh Mail\n\n Fresh Mail")
+        connection.login(user = "sender@email.com",password = "password")
+        connection.sendmail(from_addr = "sender@email.com",to_addrs = 'reciever@email.com', msg = "Subject:Very Fresh Mail\n\n Fresh Mail")
         connection.close()
         return '1'
     except:
